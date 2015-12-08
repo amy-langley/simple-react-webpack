71a1963e14b47421358f424529582c0c8545d569
Amy Boyer <amy.r.boyer@gmail.com>
2015-12-08 01:15:19 +0000

    just a little spring cleaning

--- app/components/coffeescript-component.cjsx
+++ app/components/coffeescript-component.cjsx
@@ -2,4 +2,4 @@ React = require 'react'
 
 module.exports = React.createClass
 	render: -> 
-		<li>This is a React component in CoffeeScript JSX. <em class="nice">{this.props.nice}</em></li>;
+		<li>This is a React component defined using CoffeeScript and CJSX. <em class="nice">{this.props.nice}</em></li>;
--- app/components/es6-component.js
+++ app/components/es6-component.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class ES6Component extends React.Component{
 	render(){
-		return <li>This is a React component in ES6 and JSX. <em class="nice">{this.props.nice}</em></li>;
+		return <li>This is a React component defined using ES6 and JSX. <em class="nice">{this.props.nice}</em></li>;
 	}
 }
